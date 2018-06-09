725c834997e773cb161bc125ae0c60eeb7558186
lmg <lmg1205@outlook.com>
2018-06-09 16:23:47 +0800

    增加测试实现

--- logistic.py
+++ logistic.py
@@ -5,44 +5,41 @@ import matplotlib.pyplot as plt
 class GameOfLife(object):
     def __init__(self, cellsShape):
         """
-        Parameters
-        ----------
-        cells_shape : 一个元组，表示画布的大小。
-        Examples
-        --------
-        建立一个高20，宽30的画布
-        game = GameOfLife((20, 30))
+        cellsShape : 一个元组，表示画布的大小
         """
 
         # 矩阵的四周不参与运算
         self.cells = np.zeros(cellsShape)
 
-        realWidth = cellsShape[0] - 2
-        realHeight = cellsShape[1] - 2
         self.cells[50:70, 40:45] = np.random.randint(2, size=(20, 5))
         self.cells[1:5, 1:5] = np.random.randint(2, size=(4, 4))
-        #self.cells[1:-1, 1:-1] = np.random.randint(2, size=(realWidth, realHeight))
+
         self.timer = 0
         self.mask = np.ones(9)
         self.mask[4] = 0
 
+
+
+    def is_alive(self,i,j):
+        neighbor = self.cells[i - 1:i + 2, j - 1:j + 2].reshape((-1,))
+        neighborNum = np.convolve(self.mask, neighbor, 'valid')[0]
+
+        if neighborNum == 3:
+            return 1
+        elif neighborNum == 2:
+            return self.cells[i, j]
+        else:
+            return 0
+
+
     def updateState(self):
         """更新一次状态"""
-        buf = np.zeros(self.cells.shape)
+
         cells = self.cells
         for i in range(1, cells.shape[0] - 1):
             for j in range(1, cells.shape[0] - 1):
                 # 计算该细胞周围的存活细胞数
-                neighbor = cells[i - 1:i + 2, j - 1:j + 2].reshape((-1,))
-                neighborNum = np.convolve(self.mask, neighbor, 'valid')[0]
-
-                if neighborNum == 3:
-                    buf[i, j] = 1
-                elif neighborNum == 2:
-                    buf[i, j] = cells[i, j]
-                else:
-                    buf[i, j] = 0
-        self.cells = buf
+                self.cells[i][j] = self.is_alive(i,j)
         self.timer += 1
 
     def plot_state(self):
@@ -62,7 +59,6 @@ class GameOfLife(object):
             plt.title('conveys game')
             plt.axis('off')
             plt.imshow(self.cells,cmap=plt.cm.gray)
-
             self.updateState()
             plt.pause(0.2)
         plt.ioff()
