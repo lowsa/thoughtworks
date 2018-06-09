c0727427ed012f586f1171fb676a687f488c865c
lmg <lmg1205@outlook.com>
2018-06-09 15:21:39 +0800

    增加测试实现

--- test.py
+++ test.py
@@ -1,20 +1,27 @@
 import  unittest
-
+import numpy as np
 from logistic import *
 class TestLogistic(unittest.TestCase):
-    def testUpdateState(self):
+    # 测试邻域有2个活细胞
+    def testStateNum2(self):
         game = GameOfLife(cellsShape=(4, 4))
         game.cells = np.array([[0,0,1,1],[0,1,0,0],[0,0,1,0],[0,0,1,1]])
         game.updateState()
-        # 测试邻域有2个活细胞
         self.assertEqual(1, game.cells[1, 1])
-        # 测试邻域有4个活细胞
-        self.assertEqual(0, game.cells[1, 2])
-        # 测试邻域有3个活细胞
-        self.assertEqual(1, game.cells[2, 1])
-        # 测试邻域有3个活细胞
-        self.assertEqual(1, game.cells[2, 2])
 
+    # 测试邻域有4个活细胞
+    def testStateNum4(self):
+        game = GameOfLife(cellsShape=(4, 4))
+        game.cells = np.array([[0,0,1,1],[0,1,0,0],[0,0,1,0],[0,0,1,1]])
+        game.updateState()
+        self.assertEqual(0, game.cells[1, 2])
+
+    # 测试邻域有3个活细胞
+    def testStateNum3(self):
+        game = GameOfLife(cellsShape=(4, 4))
+        game.cells = np.array([[0,0,1,1],[0,1,0,0],[0,0,1,0],[0,0,1,1]])
+        game.updateState()
+        self.assertEqual(1, game.cells[2, 1])
 
 if __name__ =='__main__':
     unittest.main()
